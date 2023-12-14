d1ec079ab2a9a859fb5a69d5fababa2ef75c201a
denys-khr <khrapov.denys@gmail.com>
2023-12-14 20:48:48 +0200

    sdf

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
-function calculateCircleArea(radius) {
-	return Math.PI * Math.pow(radius, 2);
- }
- 
- const radius = prompt("Введіть радіус кола:");
- const area = calculateCircleArea(radius);
- 
- console.log('Площа кола з радіусом' + radius + ' дорівнює: ' + area);
- 
+// function calculateCircleArea(radius) {
+// 	return Math.PI * Math.pow(radius, 2);
+// }
 
- 
+// const radius = prompt("Введіть радіус кола:");
+// const area = calculateCircleArea(radius);
+
+// console.log('Площа кола з радіусом' + radius + ' дорівнює: ' + area);
+
+
+
+function calculate(a, b, c, operation) {
+	if (operation === 'add') {
+		 console.log("Сума:", a + b + c);
+	} else if (operation === 'subtract') {
+		 console.log("Різниця:", a - b - c);
+	} else if (operation === 'multiply') {
+		 console.log("Добуток чисел:", a * b * c);
+	} else if (operation === 'divide') {
+		console.log("Ділення:", a / b / c);
+	} else {
+		 console.log("Невідома операція");
+	}
+}
+
+calculate(10, 5, 2, 'add');
+calculate(10, 5, 2, 'subtract');
+calculate(10, 5, 2, 'multiply');
+calculate(10, 5, 2, 'divide');
